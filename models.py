d24f1aa34c2c77409c467eb24bb1518382ea67b2
Topsitemakers <support@topsitemakers.com>
2013-09-12 21:04:43 +0200

    adding email column in the sample user model

--- models.py
+++ models.py
@@ -19,6 +19,7 @@ class User(Base):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(30))
 
     def __init__(self, name=None, password=None):
