aede095c114eaf183bc23936fb84a86bdd4184be
Topsitemakers <support@topsitemakers.com>
2013-09-12 21:10:01 +0200

    adding the link for generating random strings in comments (necessary for SECRET_KEY)

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Secret key for session management.
-SECRET_KEY = 'asdf'
+# Secret key for session management. You can generate random strings here:
+# http://clsc.net/tools-old/random-string-generator.php
+SECRET_KEY = 'my precious'
 
 # Connect to the database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
