d1847fbcfb5342ce4e3cff0ebac298adcdf80a4f
Vicente Romero Andrade <valdr.stiglitz@gmail.com>
2022-09-27 10:11:31 -0500

    change TextField to StringField

--- _updated/app/forms.py
+++ _updated/app/forms.py
@@ -1,13 +1,13 @@
 from flask_wtf import Form
-from wtforms import TextField, PasswordField
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, EqualTo, Length
 
 
 class RegisterForm(Form):
-    name = TextField(
+    name = StringField(
         'Username', validators=[DataRequired(), Length(min=6, max=25)]
     )
-    email = TextField(
+    email = StringField(
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
     password = PasswordField(
@@ -21,11 +21,11 @@ class RegisterForm(Form):
 
 
 class LoginForm(Form):
-    name = TextField('Username', [DataRequired()])
+    name = StringField('Username', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
 
 
 class ForgotForm(Form):
-    email = TextField(
+    email = StringField(
         'Email', validators=[DataRequired(), Length(min=6, max=40)]
     )
